bf1e4a2665ebd22ddf9392b7d1d70d30022162ca
Camila Rojas S <56646322+Camirojasserantoni@users.noreply.github.com>
2020-07-02 12:20:59 -0400

    status

--- app.js
+++ app.js
@@ -1,57 +1,83 @@
-let words = document.getElementsByClassName('word');
-let wordArray = [];
-let currentWord = 0;
 
-words[currentWord].style.opacity = 1;
-for (let i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
-
-function changeWord() {
-  let cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '<>-_\\/[]{}—=+*^?/&#________'
+    this.update = this.update.bind(this)
+  };
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 70)
+      const end = start + Math.floor(Math.random() * 70)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
   }
-  
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
   }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  let content = word.innerHTML;
-  word.innerHTML = '';
-  let letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
   }
-  
-  wordArray.push(letters);
 }
 
-changeWord();
-setInterval(changeWord, 4000);
+const phrases = [
+  'Yo soy mujer ', 
+  'Front-end Developer 👩🏻‍💻',
+  'Psicóloga', 
+  'Curiosa',
+  'Me gusta...',
+  'aprender cosas nuevas 📚',
+  'ver películas 📽️',
+  'andar en bicicleta 🚵🏻‍♀️',
+  ' y los gatos 😺',
+  
+]
+
+const el = document.querySelector('.text_container')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 2500)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
 
 
 //changed scrolling
