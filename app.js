d9df02a20242802ad5d807b58f73d3aae98f04e7
Camila Rojas S <56646322+Camirojasserantoni@users.noreply.github.com>
2020-07-02 15:37:26 -0400

    mejoras a vista  mobile

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const fx = new TextScramble(el)
 let counter = 0
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 2500)
+    setTimeout(next, 1500)
   })
   counter = (counter + 1) % phrases.length
 }
